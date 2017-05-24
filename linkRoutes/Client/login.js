8c53dbb1aaf58326d737069e211133685f544464
pucumt <weple@qq.com>
2017-05-24 23:04:38 +0800

    main page

--- linkRoutes/Client/login.js
+++ linkRoutes/Client/login.js
@@ -4,31 +4,31 @@ var crypto = require('crypto'),
     checkNotLogin = auth.checkNotLogin;
 
 module.exports = function(app) {
-    app.get('/admin/login', checkNotLogin);
-    app.get('/admin/login', function(req, res) {
-        res.render('Server/login.html', {
-            title: '登录',
-            user: req.session.admin
-        });
-    });
+    // app.get('/admin/login', checkNotLogin);
+    // app.get('/admin/login', function(req, res) {
+    //     res.render('Server/login.html', {
+    //         title: '登录',
+    //         user: req.session.admin
+    //     });
+    // });
 
-    app.post('/admin/login', checkNotLogin);
-    app.post('/admin/login', function(req, res) {
-        //生成密码的 md5 值
-        var md5 = crypto.createHash('md5'),
-            password = md5.update(req.body.password).digest('hex');
-        //检查用户是否存在
-        User.getbyName(req.body.name).then(function(user) {
-            if (!user) {
-                return res.redirect('/admin/login'); //用户不存在则跳转到登录页
-            }
-            //检查密码是否一致
-            if (user.password != password) {
-                return res.redirect('/admin/login'); //密码错误则跳转到登录页
-            }
-            //用户名密码都匹配后，将用户信息存入 session
-            req.session.admin = user;
-            res.redirect('/admin/articleList'); //登陆成功后跳转到主页
-        });
-    });
+    // app.post('/admin/login', checkNotLogin);
+    // app.post('/admin/login', function(req, res) {
+    //     //生成密码的 md5 值
+    //     var md5 = crypto.createHash('md5'),
+    //         password = md5.update(req.body.password).digest('hex');
+    //     //检查用户是否存在
+    //     User.getbyName(req.body.name).then(function(user) {
+    //         if (!user) {
+    //             return res.redirect('/admin/login'); //用户不存在则跳转到登录页
+    //         }
+    //         //检查密码是否一致
+    //         if (user.password != password) {
+    //             return res.redirect('/admin/login'); //密码错误则跳转到登录页
+    //         }
+    //         //用户名密码都匹配后，将用户信息存入 session
+    //         req.session.admin = user;
+    //         res.redirect('/admin/articleList'); //登陆成功后跳转到主页
+    //     });
+    // });
 }
--- linkRoutes/Client/logout.js
+++ linkRoutes/Client/logout.js
@@ -2,9 +2,9 @@ var auth = require("./auth"),
     checkLogin = auth.checkLogin;
 
 module.exports = function(app) {
-    app.get('/admin/logout', checkLogin);
-    app.get('/admin/logout', function(req, res) {
-        req.session.admin = null;
-        res.redirect('/admin/login'); //登出成功后跳转到登录页面
-    });
+    // app.get('/admin/logout', checkLogin);
+    // app.get('/admin/logout', function(req, res) {
+    //     req.session.admin = null;
+    //     res.redirect('/admin/login'); //登出成功后跳转到登录页面
+    // });
 }
